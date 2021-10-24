576d362a42b7ead5034adef94dcf79e5aeaed800
Alami Erhaidi <alamierhaidi@iMac-de-Alami.local>
2021-10-24 12:10:32 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {sans: ['Brown-Regular', 'sans-serif'],}
+
+    },
+  },
+  variants: {
+    extend: {
+      
+
+
+    },
+  },
+  plugins: [],
+}
